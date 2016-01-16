efca004f3f159120c626108f93af7491b7aff487
Christer Jonassen <christer@csdnserver.com>
2016-01-16 19:39:07 +0100

    Move to php app

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php include_once("apeco.html"); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>apeco/title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <style>
+			body {
+			    background-color: rgb(70,0,150);
+			}
+
+			a {
+			    color: rgb(225,229,232);
+			    text-decoration: none;
+          font-family: 'Open Sans', sans-serif;
+			}
+			a:hover {
+	  		  color: white;
+			}
+			</style>
+  </head>
+  <body>
+    <a href="http://github.com">a</a>
+    <a href="http://github.com">p</a>
+    <a href="http://github.com">e</a>
+    <a href="http://github.com">c</a>
+    <a href="http://github.com">o</a>
+  </body>
+</html>
